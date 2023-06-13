cf06c44f9b89fc53ac5147576bc02ddcba0b5ff0
Saida Lachgar <s.lachgar@picaxel.com>
2023-06-13 16:19:59 +0100

    - persist visites

--- Admin/src/app/pages/visites/visite.module.ts
+++ Admin/src/app/pages/visites/visite.module.ts
@@ -3,27 +3,29 @@ import { NgModule } from '@angular/core';
 // import { UIModule } from 'src/app/shared/ui/ui.module';
 import { visiteRoutingModule } from './visite-routing.module';
 
-import { NgbDatepickerModule, NgbPopoverModule, NgbTimepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbAlertModule, NgbDatepickerModule, NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { NgApexchartsModule } from 'ng-apexcharts';
 import { visitesListComponent } from './visites-list/visites-list.component';
-import { visiteCreateComponent } from './visite-create/visite-create.component';
-import { visiteUpdateComponent } from './visite-update/visite-update.component';
+import { visitePersistComponent } from './visite-persist/visite-persist.component';
 import { visiteDetailsComponent } from './visite-details/visite-details.component';
+import { UiSwitchModule } from 'ngx-ui-switch';
 
 @NgModule({
   imports: [
     visiteRoutingModule,
     SharedModule,
     NgbDatepickerModule,
-    NgbTimepickerModule ,
     NgbPopoverModule,
-    NgApexchartsModule
+    NgbAlertModule,
+    NgApexchartsModule,
+    UiSwitchModule.forRoot({
+      size: 'small',
+    }),
   ],
   declarations: [
     visitesListComponent,
-    visiteCreateComponent,
-    visiteUpdateComponent,
+    visitePersistComponent,
     visiteDetailsComponent,
   ],
 })
